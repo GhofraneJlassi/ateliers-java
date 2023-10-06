b25c08d4b5bc220d6d61163e786cfc86f25d979a
developobject <ghofrane.jlassi.1@ulaval.ca>
2023-10-06 12:40:52 -0400

    Create DrawingPanel.java

---
+++ gui/DrawingPanel.java
@@ -0,0 +1,53 @@
+package Ateliers.gui;
+
+import Ateliers.domain.drawing.FruitBasketDrawer;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.io.Serializable;
+import javax.swing.JPanel;
+import javax.swing.border.BevelBorder;
+
+public class DrawingPanel extends JPanel implements Serializable {
+    
+    public Dimension initialDimension;
+    private MainWindow mainWindow;
+    
+    public DrawingPanel(){
+    }
+    
+    public DrawingPanel(MainWindow mainWindow) {
+        this.mainWindow = mainWindow;
+        setBorder(new javax.swing.border.BevelBorder(BevelBorder.LOWERED));
+        int width = (int) (java.awt.Toolkit.getDefaultToolkit().getScreenSize().width);
+        setPreferredSize(new Dimension(width,1));
+        setVisible(true);
+        int height = (int)(width*0.5);
+        initialDimension = new Dimension(width,height);
+    }
+    @Override
+    protected void paintComponent(Graphics g)
+    {
+        if (mainWindow != null){
+            super.paintComponent(g); 
+            FruitBasketDrawer mainDrawer = new FruitBasketDrawer(mainWindow.controller,initialDimension);
+            mainDrawer.draw(g);
+        }
+    }
+    
+    public MainWindow getMainWindow(){
+        return mainWindow;
+    }
+    
+    public void setMainWindow(MainWindow mainWindow){
+        this.mainWindow = mainWindow;
+    }
+    
+    public Dimension getInitialDimension(){
+        return initialDimension;
+    }
+    
+    public void setInitialDimension(){
+        
+    }
+}
+
